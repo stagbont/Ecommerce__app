ca68369b6e78a8909cb8cb027a4d85548ee2b441
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-24 08:42:06 +0000

    Done With the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
+
 
 const Container = styled.div`
       height: 60;
@@ -47,7 +49,16 @@ const Logo = styled.h1`
 
 const Right = styled.div`
       flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
 `;
+
+const MenuItem = styled.div`
+      font-size: 14px;
+      cursor: pointer;
+      margin-left: 25px;
+`
 
 const Navbar = () => {
   return (
@@ -57,11 +68,19 @@ const Navbar = () => {
                   <Language>EN</Language>
                   <SearchContainer>
                         <Input/>
-                        <Search/>
+                        <Search style={{color: 'gray', fontSize: 16}}/>
                   </SearchContainer>
             </Left>
             <Center><Logo>LAMA.</Logo></Center>
-            <Right>right</Right>
+            <Right>
+                  <MenuItem>REGISTER</MenuItem>
+                  <MenuItem>SIGN IN</MenuItem>
+                  <MenuItem>
+                  <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined/>
+                  </Badge>   
+                  </MenuItem>
+            </Right>
       </Wrapper>
     </Container>
   )
