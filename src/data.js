8337658d31da92891173c10092a3c27c7b763a32
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-25 19:00:30 +0000

    Done with Categories Container

--- src/data.js
+++ src/data.js
@@ -21,3 +21,21 @@ export const sliderItems = [
         bg: "",
     },
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/KzRknbK/Print-Dress1.png",
+        title: "Print Dress"
+    },
+    {
+        id: 1,
+        img: "https://i.ibb.co/BCD01zx/Print-top1.jpg",
+        title: "Print Top"
+    },
+    {
+        id: 1,
+        img: "https://i.ibb.co/qnf0cYV/Dashiki1.png",
+        title: "Dashiki Top"
+    }
+]
