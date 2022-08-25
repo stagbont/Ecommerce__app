ca18ff5265959ad8ad072a805d9ed6ce417a13be
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-25 16:59:10 +0000

    Done with the Slider element

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/7bxpyCr/hero1.png",
+        title: "SANKOFA SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/wWHsNcP/hero2.png",
+        title: "SANKOFA SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 1,
+        img: "https://i.ibb.co/d6gMnbK/hero3.png",
+        title: "SANKOFA SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "",
+    },
+]
