7dc896e0a00208f1ca44a8db82c8adb6b88dd9d1
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-26 02:08:49 +0000

    Done with the product list page with the sorting and filter button

---
+++ src/pages/Product.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+
+const Container = styled.div``;
+
+const Product = () => {
+  return (
+    <Container>
+        <Navbar />
+        <Announcement />
+        <Newsletter />
+        <Footer />
+    </Container>
+  )
+}
+
+export default Product
