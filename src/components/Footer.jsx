5c4daf7975eabbbbe1ebcdf79d339e56f168390a
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-26 01:32:03 +0000

    Done with the footer Element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons";
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -37,9 +37,37 @@ const Center = styled.div`
     flex: 1;
     padding: 20px;
 `;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
 const Right = styled.div`
     flex: 1;
     padding: 20px;
+`;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
 `;
 
 const Footer = () => {
@@ -65,10 +93,36 @@ const Footer = () => {
                 </SocialIcon>
            </SocialContainer>
         </Left>
-        <Center></Center>
-        <Right></Right>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Man Fashion</ListItem>
+                <ListItem>Woman Fashion</ListItem>
+                <ListItem>Accessories</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+            </List>
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+            <ContactItem>
+                <Room style={{marginRight: "10px"}}/> Legon Campus
+            </ContactItem>
+            <ContactItem>
+                <Phone style={{marginRight: "10px"}}/> 0555555748
+            </ContactItem>
+            <ContactItem>
+                <MailOutline style={{marginRight: "10px"}}/> project@group1.com 
+            </ContactItem>
+            <Payment src = "https://i.ibb.co/Qfvn4z6/payment.png" />
+        </Right>
     </Container>
   )
-}
+} 
 
 export default Footer
