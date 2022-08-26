f4706266c24b15d321660488b04f719877e0f2b7
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-26 01:49:40 +0000

    On the early stages of the ProductList page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components"
+import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+
+const Container = styled.div``;
+const Title = styled.h1`
+    margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    margin: 20px;    
+`;
+
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar />
+        <Announcement />
+        <Title>Dresses</Title>
+        <FilterContainer>
+            <Filter>filter</Filter>
+            <Filter>filter2</Filter>
+        </FilterContainer>
+        <Products />
+        <Newsletter />
+        <Footer />
+    </Container>
+  )
+}
+
+export default ProductList
