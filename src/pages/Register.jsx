4b58d40ef7a81c339c0fdcb0c29117c961b06bdf
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-27 09:40:46 +0000

    Done with the Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,32 +3,48 @@ import styled from "styled-components"
 const Container = styled.div`
     width: 100vw;
     height: 100vh;
+    background: linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url("https://i.ibb.co/cbg3jFW/register-img.jpg") center;
     display: flex;
     align-items: center;
-    justify-centent: center;
+    justify-content: center;
 `;
 
 const Wrapper = styled.div`
     width: 40%;
     padding: 20px;
-    background-color: #cba696;
+    background-color: white;
 `;
 
 const Form = styled.form`
     display: flex;
-    flex-wrap: w
+    flex-wrap: wrap;
 `;
 
 const Title = styled.h1`
     font-size: 24px;
-    font-weight: 300px;
+    font-weight: 300;
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`;
 
-const Agreement = styled.span``;
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
 
 
 const Register = () => {
