d8b0a83b8631e78cc0b02838b127d69f568f87cb
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-08-27 09:51:21 +0000

    Done with the login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
-  return <Register/>;
+  return <Login/>;
 };
 
 export default App;   
